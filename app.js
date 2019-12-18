d54391562cea4c70960165faa01edea57bfa89bc
unknown <houj@oxy.edu>
2019-12-18 11:45:58 -0800

    Added modelParams to app.js

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 navigator.getUserMedia = navigator.getUserMedia
 
+const modelParams = {
+    flipHorizontal: true,   // flip e.g for video 
+    imageScaleFactor: 0.7,  // reduce input image size for gains in speed.
+    maxNumBoxes: 20,        // maximum number of boxes to detect
+    iouThreshold: 0.5,      // ioU threshold for non-max suppression
+    scoreThreshold: 0.79,    // confidence threshold for predictions.
+  }
+
 //select everything in html
 const video = document.querySelector('#video');
 const audio = document.querySelector('#audio');
@@ -11,12 +19,19 @@ handTrack.startVideo(video).then(status => {
     if(status){
         navigator.getUserMedia({video: {}}, stream =>{
             video.srcObject = stream;
+            setInterval(runDetection, 1000);
         },
         err => console.log(err))
     }
-})
+});
+
+function runDetection(){
+    model.detect(video).then(predictions=>{
+        console.log(predictions);
+    });
+}
 
 //load up model
-handTrack.load().then(lmodel =>{
-    model = lmodel
-})
+handTrack.load(modelParams).then(lmodel =>{
+    model = lmodel;
+});
